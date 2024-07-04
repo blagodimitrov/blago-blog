e3148fb20b9767542bbfa6cf5752f48be65fefd7
blagodimitrov <xarddet@gmail.com>
2024-07-04 12:07:10 +0300

    Reusing components.

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,30 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  const [name, setName] = useState('Mario');
-  const [age, setAge] = useState('25');
-
-  const handleClick = () => {
-    setName('Luigi');
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: 'Web Development',
+      body: 'lorem ipsum...',
+      author: 'Blago',
+      id: 1,
+    },
+    { title: 'React and Vite', body: 'lorem ipsum...', author: 'React', id: 2 },
+    {
+      title: 'Combining Graphic Design and Programming',
+      body: 'lorem ipsum...',
+      author: 'Blago',
+      id: 3,
+    },
+  ]);
 
   return (
     <div className='home'>
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      <BlogList blogs={blogs} title='All Blogs' />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === 'Blago')}
+        title='Blago Blogs'
+      />
     </div>
   );
 };
